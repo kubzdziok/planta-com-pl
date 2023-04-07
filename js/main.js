ed880987dd71d0d4fc8471ad89f1614410f514aa
pslowakiewicz <piotr.slowakiewicz@grupawp.pl>
2023-04-07 22:22:14 +0200

    add responsive hamburger menu

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
