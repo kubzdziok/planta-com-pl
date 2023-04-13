154b0029ce0e2d1c32c02e66e578830d1b114b20
pslowakiewicz <piotr.slowakiewicz@grupawp.pl>
2023-04-13 11:45:59 +0200

    change loader hide to fadeOut

--- js/main.js
+++ js/main.js
@@ -5,3 +5,38 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
+
+function loaderFadeOut() {
+    $(window).on('load', function () {
+        $(".loader-wrapper").fadeOut();
+    });
+}
+
+function loadOwlCarousele() {
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel(
+            {
+                loop: false,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    350: {
+                        items: 2
+                    },
+                    550: {
+                        items: 3
+                    },
+                    900: {
+                        items: 4
+                    },
+                    1024: {
+                        items: 5
+                    }
+                }
+            }
+        );
+    });
+}
